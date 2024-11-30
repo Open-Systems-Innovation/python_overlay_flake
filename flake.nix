247340d79e785cc062396c7b973208d34d3828ba
Open-Systems-Innovation <info@opensystemsinnovation.com>
2024-11-30 13:46:10 -0500

    fixed wrong name in flake overlay

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     in
     {
       overlay.default = final: prev: {
-        hi-there-world = prev.callPackage ./pkgs/hi-there-world { };
+        simple_python_package = prev.callPackage ./pkgs/hi-there-world { };
       };
       
       packages.${system} = {
